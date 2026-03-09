a79bd59ea59f3c0c06eef195c06a871236505f2b
lostan89 <skeeperenator@gmail.com>
2026-03-09 22:34:08 +0300

    add ending for winery_age_years

--- main.py
+++ main.py
@@ -3,15 +3,29 @@ import datetime
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+def ending(num):
+    num = num % 100
+    if 5 <= num <= 20:
+        return 'лет'
+    num = num % 10
+    if num == 1:
+        return 'год'
+    elif 2 <= num <= 4:
+        return 'года'
+    else:
+        return 'лет'
+
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
 template = env.get_template('template.html')
-
+winery_age_years=datetime.datetime.now().year-1920
+age_ending=ending(winery_age_years)
 rendered_page = template.render(
-    winery_age_years=datetime.datetime.now().year-1920,
+    winery_age_years=winery_age_years, 
+    age_ending=age_ending,
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
