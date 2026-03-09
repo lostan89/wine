d47d01a2e31b7f0f3271601f2bd77b550bb36de6
lostan89 <skeeperenator@gmail.com>
2026-03-09 22:07:35 +0300

    add winery ages

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    winery_age_years=datetime.datetime.now().year-1920,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('127.0.0.1', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
