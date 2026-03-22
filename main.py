5885b3ff7cfe6bdff5ab4b50b30f4dcc8dbf862a
lostan89 <skeeperenator@gmail.com>
2026-03-22 13:07:54 +0300

    add ifmain block

--- main.py
+++ main.py
@@ -18,31 +18,35 @@ def ending(num):
     else:
         return "лет"
 
+def main():
 
-env = Environment(
-    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
-)
-
-
-df = pd.read_excel("wine3.xlsx", keep_default_na=False)
+    env = Environment(
+        loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+    )
 
 
-wine_info_list = collections.defaultdict(list)
+    df = pd.read_excel("wine3.xlsx", keep_default_na=False)
 
-for category, group_df in df.groupby("Категория"):
-    wine_info_list[category] = group_df.to_dict("records")
 
-template = env.get_template("template.html")
-winery_age_years = datetime.datetime.now().year - 1920
-age_ending = ending(winery_age_years)
-rendered_page = template.render(
-    winery_age_years=winery_age_years,
-    age_ending=age_ending,
-    wine_info_list=list(wine_info_list.values()),
-)
+    wine_info_list = collections.defaultdict(list)
 
-with open("index.html", "w", encoding="utf8") as file:
-    file.write(rendered_page)
+    for category, group_df in df.groupby("Категория"):
+        wine_info_list[category] = group_df.to_dict("records")
 
-server = HTTPServer(("127.0.0.1", 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    template = env.get_template("template.html")
+    winery_age_years = datetime.datetime.now().year - 1920
+    age_ending = ending(winery_age_years)
+    rendered_page = template.render(
+        winery_age_years=winery_age_years,
+        age_ending=age_ending,
+        wine_info_list=list(wine_info_list.values()),
+    )
+
+    with open("index.html", "w", encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(("127.0.0.1", 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
